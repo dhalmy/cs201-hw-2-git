abec58d1c9d6182fb1aa876905b3e635968ef22e
david <david@104.194.114.68>
2021-09-27 14:34:26 -0500

    commit #1

---
+++ Homework2-cs201/src/homework2/Main.java
@@ -0,0 +1,111 @@
+package homework2;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		String fn, String ln, int id, double gpa, boolean hol, int gradeCnt, double rawG)
+		
+		Student student1 = new Student("Joe", "Smith", 12345, 0.0, false, 0, 0.0);
+		Student student2 = new Student("Jane", "Doe", 54321, 4.0, false, 1, 100.0);
+		Student student3 = new Student("Mary", "Sue", 12321, 3.0, false, 2, 160.0);
+		Student student4 = new Student("Sam", "Brown", 11111, 2.0, false, 3, 225.0);
+		Student student5 = new Student("Adam", "Lee", 10001, 3.0, false, 5, 435.0);
+		
+		Student[] students = new Student[5];
+		students[0] = student1;
+		students[1] = student2;
+		students[2] = student3;
+		students[3] = student4;
+		students[4] = student5;
+		
+		System.out.println("Student 1 has a gpa of " + student1.getGpa() + " and is " + student1.holdCheck()); //student1 has no grades in the gradebook yet, so he isn't placed on hold despite having a 0.0 gpa
+		System.out.println("Student 1 got a new grade in and now has a gpa of " + student1.gpaUpdate(90));
+		System.out.println("Student 1 got a new grade in and now has a gpa of " + student1.gpaUpdate(59));
+		System.out.println("Student 1 got a new grade in and now has a gpa of " + student1.gpaUpdate(100));
+		System.out.println("Student 1 has a gpa of " + student1.getGpa() + " and is " + student1.holdCheck()); //student1 has a sub 3.0 gpa and is placed on hold
+		
+		
+		//3. Sort a set of student objects
+		//By name
+		Student.display(students);
+		Student.sortByName(students);
+		System.out.println("Sorted by last name");
+		Student.display(students);
+		//By student ID
+		Student.sortById(students);
+		System.out.println("Sorted by ID #");
+		Student.display(students);
+		//By GPA (high to low)
+		Student.sortByGpa(students);
+		System.out.println("Sorted by GPA");
+		Student.display(students);
+		
+		//1. Sort a random set of integers
+		System.out.println();
+		System.out.println("_____________________________");
+		System.out.println("Sorting a random set of new integers");
+		System.out.print("Random set of integers: ");
+		int randInts[] = new int[5];
+		for(int i = 0; i < 5; i++) {
+		randInts[i] = (int)(Math.random()*10);
+		System.out.print(randInts[i] + " ");
+			}
+		System.out.println("Sorting random integers high to low:");
+		sortIntAsc(randInts);
+		System.out.println(Arrays.toString(randInts));
+		
+		System.out.println("Sorting random integers low to high:");
+		sortIntDec(randInts);
+		System.out.println(Arrays.toString(randInts));
+		
+		for(int i = 0; i < 50; i++) {
+//			char randChar = (char) ((int)(Math.random()*100)%94+33);
+//			System.out.println( (99%94));
+//			System.out.println(randChar);
+			System.out.println((Math.random()*100));
+			System.out.println((Math.random()*100)%94);
+			System.out.println((Math.random()*100)%94+33);
+			System.out.println();
+		}
+		
+		
+		
+		
+
+ }
+	//sorts randInts ascending
+	public static void sortIntAsc(int[] rand) {
+	int min;
+	for(int i = 0; i < rand.length-1; i++) {
+		min = i;
+		for(int j = i + 1; j < rand.length; j++) {
+			if(rand[j] > rand[min]) min = j;
+		}
+		int temp = rand[i];
+		rand[i] = rand[min];
+		rand[min] = temp;
+	  }
+	}
+	
+	//sorts randInts descending
+		public static void sortIntDec(int[] rand) {
+		int min;
+		for(int i = 0; i < rand.length-1; i++) {
+			min = i;
+			for(int j = i + 1; j < rand.length; j++) {
+				if(rand[j] < rand[min]) min = j;
+			}
+			int temp = rand[i];
+			rand[i] = rand[min];
+			rand[min] = temp;
+		  }
+		}
+	
+	
+	
+	
+}
+	
+
---
+++ Homework2-cs201/src/homework2/Student.java
@@ -0,0 +1,172 @@
+package homework2;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	private int id;
+	private double gpa;
+	private boolean isHold;
+	private int gradeCnt;
+	private double rawGrade;
+	private double convGrade;
+	private String fullName;
+	
+//	Create setName method which takes 3 string inputs and assigns the firstName, lastName, and idNum values to their respective inputs
+//	Create updateGPA method which takes 
+//	Create placeHold method which has an if statement saying if gpa < 3.0 then set isOnHold to true
+
+	public Student(String fn, String ln, int id, double gpa, boolean hol, int gradeCnt, double rawG) {
+		firstName = fn;
+		lastName = ln;
+		this.id = id;
+		this.gpa = gpa;
+		isHold = hol;
+		this.gradeCnt = gradeCnt;
+		rawGrade = rawG;
+		fullName = ln + ", " + fn;
+	}
+	
+	public double getGpa() {
+		return gpa;
+	}
+	
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public boolean isHold() {
+		return isHold;
+	}
+
+	public void setHold(boolean isHold) {
+		this.isHold = isHold;
+	}
+
+	public int getGradeCnt() {
+		return gradeCnt;
+	}
+
+	public void setGradeCnt(int gradeCnt) {
+		this.gradeCnt = gradeCnt;
+	}
+
+	public double getRawGrade() {
+		return rawGrade;
+	}
+
+	public void setRawGrade(double rawGrade) {
+		this.rawGrade = rawGrade;
+	}
+
+	public double getConvGrade() {
+		return convGrade;
+	}
+
+	public void setConvGrade(double convGrade) {
+		this.convGrade = convGrade;
+	}
+
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+
+	public double gpaUpdate(double gradeInput) { //inputs grades based on the 100 point scale
+		rawGrade += gradeInput;
+		gradeCnt++;
+		convGrade = (rawGrade / gradeCnt) - 60; //sets up for gpa scale of 60-69 = 1.0, 70-79 = 2.0, 80-89 = 3.0, 90-100 = 4.0
+		
+		if(convGrade>=30) gpa = 4.0;
+		else if(convGrade>=20) gpa = 3.0;
+		else if(convGrade>=10) gpa = 2.0;
+		else if(convGrade>=0) gpa = 1.0;
+		else if(convGrade<0) gpa = 0.0;
+		holdCheck();
+		return gpa;
+	}
+	
+	public String holdCheck() {
+		if(gpa<3.0 && gradeCnt != 0) isHold = true;
+		else isHold = false;
+		if(isHold) return "is on hold";
+		else return "is not on hold";
+	}
+	
+	public static void sortByName(Student arr[]) {
+		int min;
+		for(int i = 0; i < arr.length-1; i++) {
+			min = i;
+			for(int j = i + 1; j < arr.length; j++) {
+				if((arr[j].getFullName()).compareTo(arr[min].getFullName()) < 0) min = j;
+			}
+			Student temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp;
+			
+		}
+	}
+	
+	public static void sortById(Student arr[]) {
+		int min;
+		for(int i = 0; i < arr.length-1; i++) {
+			min = i;
+			for(int j = i + 1; j < arr.length; j++) {
+				if(arr[j].getId() < arr[min].getId()) min = j;
+			}
+			Student temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp;
+		}
+	}
+	
+	public static void sortByGpa(Student arr[]) {
+		int min;
+		for(int i = 0; i < arr.length-1; i++) {
+			min = i;
+			for(int j = i + 1; j < arr.length; j++) {
+				if(arr[j].getGpa() > arr[min].getGpa()) min = j;
+			}
+			Student temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp;
+		}
+	}
+	
+	public static void display(Student arr[]) {
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i].toString());
+		}
+	}
+	
+	public String toString() {
+		return "Name: " + fullName + " \t | ID: " + id + "\t| GPA: " + getGpa() + "\t| Hold Status: " + holdCheck(); //add gpa before hold status
+	}
+
+}
